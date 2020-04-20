26795d7a9bfc22c325ffa4d0440191b7e9a30168
Bruno da Silva <brunocarreiro@gmail.com>
2020-04-20 11:47:49 -0700

    Spliting up the functions in two files

--- sample_tests.py
+++ sample_tests.py
@@ -1,39 +1,41 @@
 import pytest 
+import sample_functions
 
-def sum(a, b):
-    return a + b
-
-def contains_numbers(alpha_num_str):
-    for char in alpha_num_str:
-        if char.isdigit():
-            return True 
-    return False
 
 def test_sum():
     num1 = 5
     num2 = 10
     expected = 15
-    assert sum(num1, num2) == 15
+    assert sample_functions.sum(num1, num2) == 15
 
 def test_contains_numbers():
     input_str = "el12lk3j5mnfadf"
-    assert contains_numbers(input_str) == True
+    assert sample_functions.contains_numbers(input_str) == True
 
 def test_does_not_contain_numbers():
     input_str = "lkqwjqlkjlkjed"
-    assert contains_numbers(input_str) == False
-
-def div(a, b):
-    return a / b
+    assert sample_functions.contains_numbers(input_str) == False
 
 def test_div():
     num1 = 10
     num2 = 5
     expected = 2
-    assert div(num1, num2) == expected
+    assert sample_functions.div(num1, num2) == expected
 
 def test_div_by_zero():
     num1 = 10
     num2 = 0
     with pytest.raises(ZeroDivisionError):
-        div(num1, num2)
+        sample_functions.div(num1, num2)
+
+#Create separate and independent test cases
+#For instance, avoid this:
+def test_div2():
+    num1 = 10
+    num2 = 5
+    expected = 2
+    assert sample_functions.div(num1, num2) == expected
+    num2 = 0
+    with pytest.raises(ZeroDivisionError):
+        sample_functions.div(num1, num2)
+#Two aspects being tested under the same test case. Not a good practice.
